2e17b3189737701c5191081915436879b86f3159
carlyldn <88619398+carlyldn@users.noreply.github.com>
2022-03-05 22:15:45 +0000

    updated styles

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
 // A $( document ).ready() block.
+// $( document ).ready(function() {
+//     $('.menu-container').click(function(){
+//         $('.menu').fadeToggle(600);
+//         $('.cross').show();
+//         $('.menu-button').hide();
+//     $('.cross').click(function(){
+//         $('.menu').fadeToggle(600);
+//         $('.menu-button').show();
+//         $('.cross').hide();
+//     })});
+
 $( document ).ready(function() {
-    $('.menu-container').click(function(){
-        $('.menu').fadeToggle(600);
-        $('.cross').show();
-        $('.menu-button').hide();
-    $('.cross').click(function(){
-        $('.menu').fadeToggle(600);
-        $('.menu-button').show();
-        $('.cross').hide();
-    })});
+$('.menu-container').click(function(){
+    $('.menu').fadeToggle(600);
+    $('.cross').show();
+    $('.menu-button').hide();
+});
+
+$('.cross').click(function(){
+    $('.menu').fadeToggle(600);
+    $('.menu-button').show();
+    $('.cross').hide();
+});
 
     // Animate on scroll
     AOS.init();
 });
 
     // Typing Animation
-
+    // If the #typed element comes into the viewport, this callback will be run
+const viewportObserver = new IntersectionObserver((entries, observer) => {
+    // If #typed is within the viewport
+    if (entries[0].isIntersecting) {
     var typed = new Typed('#typed', {
         stringsElement: '#typed-strings',
+        showCursor: false,
         typeSpeed: 70,
         backDelay: 1000,
         backSpeed: 10,
         loop: false
     });
+    observer.unobserve(document.querySelector('#typed'))
+  }
+})
+// Instruct the IntersectioObserver to observe the #typed element
+viewportObserver.observe(document.querySelector('#typed'))
 
 
-lightGallery(document.getElementById('lightgallery'), {
-    speed: 500,
-    download: false
-});
+// lightGallery(document.getElementById('lightgallery'), {
+//     speed: 500,
+//     download: false
+// });
