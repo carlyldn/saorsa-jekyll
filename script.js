d6220bbea1dcbc41daac2eb64fa4f747d7849c0f
carlyldn <88619398+carlyldn@users.noreply.github.com>
2022-04-13 15:52:54 +0100

    updated mobile responsiveness on home page

--- script.js
+++ script.js
@@ -23,6 +23,20 @@ $('.menu-container').click(function(){
     AOS.init();
 });
 
+// Slider Gallery
+$('.slider-container').slick({
+	autoplay: true, // Do we want it to autoplay? true or false
+	autoplaySpeed: 2000, // How long between each slide when auto-playing
+	speed: 500, // How fast is the transition in milliseconds
+	arrows: true, // Do you want to show arrows to trigger each slide
+	accessibility: true, // Enables keyboard tabbing and arrow key navigation
+	dots: true, // Enables the dots below to show how many slides
+	fade: false, // Changes the animate from slide to fade if true
+	infinite: true, // When true, means that it will scroll in a circle
+	pauseOnHover: true, // When true means the autoplay pauses when hovering
+	pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+});
+
     // Typing Animation
     // If the #typed element comes into the viewport, this callback will be run
 const viewportObserver = new IntersectionObserver((entries, observer) => {
