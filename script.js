67fc50f7ddedff6bc7e242669a00f65698cf1fcc
carlyldn <88619398+carlyldn@users.noreply.github.com>
2022-06-30 20:53:56 +0100

    added privacy policy

--- script.js
+++ script.js
@@ -84,3 +84,29 @@ for (i=0; i<accordion.length; i++) {
     this.classList.toggle('active')
   })
 }
+
+// Cookie banner 
+
+// Key under which name the cookie is saved
+const cookieName = 'cookieconsent';
+// The value could be used to store different levels of consent
+const cookieValue = 'dismissed';
+
+function dismiss() {
+    const date = new Date();
+    // Cookie is valid 1 year: now + (days x hours x minutes x seconds x milliseconds)
+    date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+    // Set cookie
+    document.cookie = `${cookieName}=${cookieValue};expires=${date.toUTCString()};path=/`;
+
+    // Remove the banner
+    document.querySelector('.cookie-banner').remove();
+}
+
+// Get button element
+const buttonElement = document.querySelector('.cookie-dismiss');
+// Maybe cookie consent is not present
+if (buttonElement) {
+    // Listen on button click
+    buttonElement.addEventListener('click', dismiss);
+}
